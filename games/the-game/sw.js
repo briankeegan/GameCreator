28a58deb6677a06ee9199278f19ddc99665fb75d
bramp <32174215+briankeegan@users.noreply.github.com>
2026-08-19 08:22:49 -0700

    Create game: Newsey-the-game

---
+++ games/the-game/sw.js
@@ -0,0 +1,14 @@
+importScripts("../../shared/sw-core.js");
+
+GCRegisterServiceWorker("the-game-v2", [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "../../shared/nav.js",
+  "../../shared/nav.css",
+  "../../shared/pwa.js",
+  "../../shared/storage.js",
+  "./icons/icon.svg",
+]);
